3c871387a3cb4f4fab819e470f670e786ed35543
JoniBach <james@jamescrook.uk>
2021-01-24 16:03:57 +0000

    Adding mock data and mapping it

---
+++ src/products.js
@@ -0,0 +1,78 @@
+export const products = [
+    {
+        id: '001',
+        title: 'HP 250 G7', 
+        subTitle: 'Laptop',
+        image: 'https://www.laptopsdirect.co.uk/Images/6BP88EA_1_Classic.jpg?v=1&width=200&height=200&switchoff=enabled',
+        details: [
+            {
+                label: 'CPU',
+                content: 'Core i5-8265U '
+            },
+            {
+                label: 'RAM',
+                content: '8GB'
+            },
+            {
+                label: 'SSD',
+                content: '256GB '
+            },
+            {
+                label: 'SCREEN',
+                content: '15.6"'
+            },
+        ]
+    },
+    {
+        id: '002',
+        title: 'Lenovo V15=IIL', 
+        subTitle: 'Laptop',
+        image: 'https://www.laptopsdirect.co.uk/Images/82C50075UK_1_classic.jpg?v=3&width=200&height=200&switchoff=enabled',
+        details: [
+            {
+                label: 'CPU',
+                content: 'Core i5-1035G1 '
+            },
+            {
+                label: 'RAM',
+                content: '8GB'
+            },
+            {
+                label: 'SSD',
+                content: '256GB '
+            },
+            {
+                label: 'SCREEN',
+                content: '15.6"'
+            },
+        ]
+    },
+    {
+        id: '003',
+        title: 'MSI GF63 Thin', 
+        subTitle: 'Gaming Laptop',
+        image: 'https://www.laptopsdirect.co.uk/Images/9S7-16R412-1069_1_Supersize.png?width=937&height=937&v=3',
+        details: [
+            {
+                label: 'CPU',
+                content: 'Core i5-9300H '
+            },
+            {
+                label: 'GPU',
+                content: 'GeForce GTX 1650 Ti 4GB'
+            },
+            {
+                label: 'RAM',
+                content: '8GB'
+            },
+            {
+                label: 'SSD',
+                content: '256GB '
+            },
+            {
+                label: 'SCREEN',
+                content: '15.6"'
+            },
+        ]
+    },
+]
--- src/views/productListView.jsx
+++ src/views/productListView.jsx
@@ -1,11 +1,11 @@
 import { Card, CardContent } from "../components/card/card";
 import { ProductList, ProductListItem } from "../components/list/list";
-
+import { products } from "../products";
 export function ProductListView() {
   //   let match = useRouteMatch();
   return (
     <div>
-      <Card
+      {/* <Card
         hiddenContent={
           <div>
             <div>hi</div>
@@ -17,18 +17,21 @@ export function ProductListView() {
       >
         <CardContent src="https://api.time.com/wp-content/uploads/2016/03/national-puppy-day.jpg" />
         <CardContent>hi</CardContent>
-      </Card>
-      <ProductList>
-        <ProductListItem
-          p={10}
-          src="https://images.fineartamerica.com/images-medium-large-5/a-basket-of-puppies-susan-schmitz.jpg"
-          link={`/product/${1}`}
-        >
-          <h5>Product title</h5>
-          <h2>Subtitle</h2>
-          <p>details</p>
+      </Card> */}
+      {products.map((p, i) => (
+        <ProductListItem p={10} src={p.image} link={`/product/${p.id}`}>
+          <h5 style={{marginBlock: 5}}>{p.subTitle}</h5>
+          <h2 style={{marginBlock: 5}}>{p.title}</h2>
+          {p.details.map((d, i) => (
+            <ul>
+              <li>
+                <p style={{marginBlock: 5}}>{d.content} {d.label}</p>
+              </li>
+            </ul>
+          ))}
         </ProductListItem>
-      </ProductList>
+      ))}
+      <ProductList></ProductList>
     </div>
   );
 }
